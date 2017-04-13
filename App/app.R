fb879270f32dc3c782bf2e164db2dedae7dc5997
amycrawford <amycrawford@mehulick.student.iastate.edu>
2017-04-13 17:05:37 -0500

    Added Efficiency Range Slider

--- App/app.R
+++ App/app.R
@@ -11,6 +11,23 @@ ui <- fluidPage(
 
   mainPanel(
     plotlyOutput("plot_ferment")
+  ),
+  
+  
+  fluidRow(
+    column(4,
+           
+           # Copy the line below to make a slider range 
+           sliderInput("EFFrange", label = h3("Efficiency Range"), min = 43, 
+                       max = 89, value = c(44, 89))
+    )
+  ),
+  
+  hr(),
+  
+  fluidRow(
+    column(4, verbatimTextOutput("value")),
+    column(4, verbatimTextOutput("range"))
   )
 )
 
@@ -18,11 +35,11 @@ server <- function(input, output, session) {
   beer=read.csv("../data/beer.csv", stringsAsFactors = F)
   
   select_type=reactive({
-    beer %>% filter(Fermentation==input$FermentationType)
+    beer %>% filter(Fermentation==input$FermentationType[1] | Fermentation == input$FermentationType[2], Efficiency > input$EFFrange[1] & Efficiency < input$EFFrange[2])
     })
   
   output$plot_ferment <- renderPlotly({
-     ggplot(data=select_type(), aes(x=Calories, y=ABV, labels = Brand), tooltip = c("Brand"))+geom_point()
+     ggplot(data=select_type(), aes(x=Calories, y=ABV, labels = Brand), tooltip = c("Brand"))+geom_point() + ylim(2,7) + xlim(50,300)
     
   })
     
